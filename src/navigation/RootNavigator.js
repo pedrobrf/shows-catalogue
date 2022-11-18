400b29dacd5718a34d27623d1c2cdadac77469b0
Pedro <ptr.bushi@hotmail.com>
2022-11-17 23:48:47 -0300

    feat: change navigation

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import SearchScreen from '../screens/Search';
+import ResultsScreen from '../screens/Results';
 
 import HeaderLeft from '../components/HeaderLeft';
-import TabIcon from '../components/TabBarIcon';
-
-import { activeIcon, inactiveIcon } from '../constants/colors';
 import { SCREEN_NAMES } from '../constants/texts';
 
 const SearchStack = createNativeStackNavigator();
 
 const SearchStackScreen = () => (
 	<SearchStack.Navigator
-		screenOptions={{
+		screenOptions={navigation => ({
 			headerBackTitleVisible: false,
-			headerLeft: ({ onPress, canGoBack }) =>
-				canGoBack ? <HeaderLeft onPress={onPress} /> : null
-		}}
+			headerLeft: ({ canGoBack }) =>
+				canGoBack ? (
+					<HeaderLeft onPress={navigation.navigation.goBack} />
+				) : null
+		})}
 	>
 		<SearchStack.Screen
 			name="search"
@@ -28,51 +27,15 @@ const SearchStackScreen = () => (
 		/>
 		<SearchStack.Screen
 			name="results"
-			component={() => null}
+			component={ResultsScreen}
 			options={{ title: SCREEN_NAMES.results }}
 		/>
 	</SearchStack.Navigator>
 );
 
-const FavesStack = createNativeStackNavigator();
-
-const FavesStackScreen = () => (
-	<FavesStack.Navigator>
-		<FavesStack.Screen
-			name="favorites"
-			component={() => null}
-			options={{ title: SCREEN_NAMES.favorites }}
-		/>
-	</FavesStack.Navigator>
-);
-
-const Tab = createBottomTabNavigator();
-
 const Navigator = ({}) => (
 	<NavigationContainer>
-		<Tab.Navigator
-			tabBarOptions={{
-				activeTintColor: activeIcon,
-				activeBackgroundColor: activeIcon + '40',
-				inactiveTintColor: inactiveIcon,
-				inactiveBackgroundColor: activeIcon + '10'
-			}}
-			screenOptions={({ route }) => ({
-				tabBarIcon: ({ focused, color, size }) => (
-					<TabIcon
-						focused={focused}
-						color={color}
-						size={size}
-						route={route}
-					/>
-				),
-				title: SCREEN_NAMES[route.name],
-				headerShown: false
-			})}
-		>
-			<Tab.Screen name="Search" component={SearchStackScreen} />
-			<Tab.Screen name="Favorites" component={FavesStackScreen} />
-		</Tab.Navigator>
+		<SearchStackScreen />
 	</NavigationContainer>
 );
 
